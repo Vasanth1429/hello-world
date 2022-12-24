b2f7a4d4690e1bdd369acc348792909f8c2f8a09
Vasanth <vasanthkanuri1429@gmail.com>
2022-12-24 16:36:51 +0530

    initial commit

---
+++ public/script.js
@@ -0,0 +1,19 @@
+if ('WebSocket' in window) {
+    // It means your browser supoort WebSocket
+    var ws = new WebSocket('ws://localhost:8000')
+  } else {
+    alert(
+      'WebSocket is not supported by your Browser, please use any other browser'
+    )
+  }
+  
+  function clicked() {
+    const input = document.getElementById('angle').value
+    const intInput = Number.parseInt(input)
+  
+    if (isNaN(intInput) == false) {
+      ws.send(intInput)
+    } else {
+      alert('Please Enter A valid Number')
+    }
+  }
